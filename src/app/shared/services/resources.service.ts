8eacc3f783c65521a075cd543627d93ac361ba3f
Yurii Tykhovskyi <yuriity@outlook.com>
2025-07-09 15:00:58 +0300

    fix: correct path for resources data source

--- src/app/shared/services/resources.service.ts
+++ src/app/shared/services/resources.service.ts
@@ -6,7 +6,7 @@ import { Resource } from '../../shared/models/resource';
   providedIn: 'root',
 })
 export class ResourcesService {
-  private resourcesDataSource = '/data/en-US_resources.json';
+  private resourcesDataSource = 'data/en-US_resources.json';
   private resourcesSignal = signal<Resource[]>([]);
   private isLoading = signal(false);
   private error = signal<Error | null>(null);
